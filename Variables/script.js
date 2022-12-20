eb5f6400e851fd5f2cd8757061fb8b1e9d0977a9
chamisupu <chami2170@gmail.com>
2022-12-20 09:51:10 +0530

    first commit

---
+++ Variables/script.js
@@ -0,0 +1,16 @@
+let showThis = "amazing";
+
+console.log(showThis);
+
+let firstName = "Chamindu";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "programmer";
+let myCurrentJob = "Gamer";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+    
