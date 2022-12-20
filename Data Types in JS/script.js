4507e21b1e8184b0760c681f8e9105b0f800f98b
chamisupu <chami2170@gmail.com>
2022-12-20 10:58:23 +0530

    first commit

---
+++ Data Types in JS/script.js
@@ -0,0 +1,21 @@
+let firstName = "Chamindu";     //string
+console.log(firstName);
+console.log(typeof firstName);
+
+let myBirthday = 2000;          //Number
+console.log(myBirthday);
+console.log(typeof myBirthday);
+
+let javaScriptIsFun = true;         //Boolean
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+
+let year;                       //Undefined
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(null);                 //Null
+console.log(typeof null);
